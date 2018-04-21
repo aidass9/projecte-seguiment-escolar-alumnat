763d134090cbb2618bba490f41635ff04efe114c
aidass9 <aida9.soriano@gmail.com>
2018-04-21 11:16:41 +0200

    Configuración Application>config>config.php y databasse.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/proyectoCI/';
+$config['base_url'] = 'http://localhost/';
 
 
 $config['index_page'] = '';
